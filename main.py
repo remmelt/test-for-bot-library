a093ecc8190b5a4a3ddd1c3c782b771d6e39fa16
Remmelt Pit <remmelt.pit@source.ag>
2022-10-24 12:52:41 +0200

    autoWIP

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
 
-r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-print(f"hello world: {r.status_code}")
+
+def doe_je_ding():
+    r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+    print(f"hello world: {r.status_code}")
